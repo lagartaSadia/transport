73585fc213d34e201c4437b067804818f0c267cd
Lucas Eduardo Campolino <campolino.l@gmail.com>
2022-05-24 03:55:27 -0300

    Validações no modulo Vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,11 @@
 class Vehicle < ApplicationRecord
   belongs_to :carrier
+
+  validates :license_plate, :brand, :vehicle_type, :fabrication_date, :capacity, presence: true
+
+  validates :license_plate, uniqueness: true
+  validates :license_plate, length: { is: 7 }
+
+  validates :fabrication_date, :capacity, numericality: { only_integer: true }
+  validates :fabrication_date, length: { is: 4 }
 end
